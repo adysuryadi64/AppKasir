4906826d63afd4fa07f956408f1c92525e8cef8f
Developer <dev@appkasir.local>
2026-05-20 03:17:42 +0700

    Release v15.2026.520.4

--- AppKasir/My Project/AssemblyInfo.vb
+++ AppKasir/My Project/AssemblyInfo.vb
@@ -29,8 +29,17 @@ Imports System.Runtime.InteropServices
 
 ' Versi di-generate otomatis saat build via Pre-build Event
 ' Format: 15.YYYY.M.D[.Rev] — jangan ubah manual
-<Assembly: AssemblyVersion("15.2026.519.8")>
-<Assembly: AssemblyFileVersion("15.2026.519.8")>
+<Assembly: AssemblyVersion("15.2026.520.4")>
+<Assembly: AssemblyFileVersion("15.2026.520.4")>
+
+
+
+
+
+
+
+
+
 
 
 
